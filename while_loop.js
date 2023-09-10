947c6b5cbbe371d526e2e655a364f0a1fb8fac63
Azhar Ali <99320098+azharalimirjat@users.noreply.github.com>
2023-09-10 17:47:10 +0500

    Add files via upload

---
+++ while_loop.js
@@ -0,0 +1,32 @@
+/*
+While Loop:
+    The while loop loops through a block of code as long as a specified condition is true.
+
+Syntax:
+while (condition) {
+  // code block to be executed
+}
+*/
+
+
+// Example:01:
+let i = 0;
+while (i < 10) {
+    console.log("The number is " + i);
+    i++;
+}
+
+
+
+console.log("=================")
+
+
+
+// Example:02:
+let j = 1, n = 5;
+
+// while loop from j = 1 to 5
+while (j <= n) {
+    console.log("Printing ",j);
+    j += 1;
+}
